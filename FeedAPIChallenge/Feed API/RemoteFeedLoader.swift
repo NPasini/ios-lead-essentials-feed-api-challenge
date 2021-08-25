97ce2c8944ddf36250dc0669d257fdab7299a5eb
Nicolò Pasini <nicolo.pasini@igenius.ai>
2021-08-26 01:00:27 +0200

    Using a variable for success code

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -5,6 +5,8 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
+	private static var OK_200: Int { return 200 }
+
 	private let url: URL
 	private let client: HTTPClient
 
@@ -24,7 +26,7 @@ public final class RemoteFeedLoader: FeedLoader {
 
 			switch result {
 			case let .success((data, response)):
-				if response.statusCode == 200 {
+				if response.statusCode == RemoteFeedLoader.OK_200 {
 					completion(FeedImageMapper.map(data: data))
 				} else {
 					completion(.failure(Error.invalidData))
