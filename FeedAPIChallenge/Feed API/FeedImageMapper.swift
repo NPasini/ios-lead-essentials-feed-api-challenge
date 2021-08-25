96ff1dac44b5d8968ea178fc8e58496f680ae88e
Nicolò Pasini <nicolo.pasini@igenius.ai>
2021-08-26 00:54:11 +0200

    Created FeedImageMapper to decode and map items from API response to FeedImage

---
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -0,0 +1,45 @@
+//
+//  FeedImageMapper.swift
+//  FeedAPIChallenge
+//
+//  Created by Nicolò Pasini on 26/08/21.
+//  Copyright © 2021 Essential Developer Ltd. All rights reserved.
+//
+
+import Foundation
+
+internal struct FeedImageMapper: Decodable {
+	private struct Root: Decodable {
+		let items: [Image]
+
+		var images: [FeedImage] {
+			return items.map { $0.image }
+		}
+	}
+
+	private struct Image: Decodable {
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case url = "image_url"
+			case location = "image_loc"
+			case description = "image_desc"
+		}
+
+		let id: UUID
+		let url: URL
+		let location: String?
+		let description: String?
+
+		var image: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: url)
+		}
+	}
+
+	internal static func map(data: Data) -> FeedLoader.Result {
+		guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			return .failure(RemoteFeedLoader.Error.invalidData)
+		}
+
+		return .success(root.images)
+	}
+}
