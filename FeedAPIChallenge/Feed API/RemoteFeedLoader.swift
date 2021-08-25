e40ef0081ac4a564be3ee208b21641ce7719a52c
Nicolò Pasini <nicolo.pasini@igenius.ai>
2021-08-26 00:56:01 +0200

    Handled client successful response in the remote feed loader by mapping data into FeedImage models

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,8 +21,10 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				if response.statusCode != 200 {
+			case let .success((data, response)):
+				if response.statusCode == 200 {
+					completion(FeedImageMapper.map(data: data))
+				} else {
 					completion(.failure(Error.invalidData))
 				}
 			case .failure:
